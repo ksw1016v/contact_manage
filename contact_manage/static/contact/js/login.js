62fa57b854e6ada22be43b163b47e1b599f7acbb
ksw <ksw1016v@gmail.com>
2020-02-07 14:04:20 +0900

    login css

--- contact_manage/static/contact/js/login.js
+++ contact_manage/static/contact/js/login.js
@@ -1,5 +1,5 @@
-const signUpButton = document.getElementById('signUp');
-const signInButton = document.getElementById('signIn');
+const signUpButton = document.getElementById('signInsignUp');
+const signInButton = document.getElementById('signUp');
 const container = document.getElementById('container');
 
 signUpButton.addEventListener('click', () => {
