431b276f7b45aae415dbc126d0d5d1fe40b0f42a
ksw <ksw1016v@gmail.com>
2020-02-08 15:26:24 +0900

    sign in & sign up

--- contact_manage/contact/urls.py
+++ contact_manage/contact/urls.py
@@ -4,11 +4,11 @@ from .views import *
 
 
 urlpatterns = [
-    path('login/',  Index.as_view(template_name='contact/login.html'), name='Index'),
-    path('logout/',  Index.as_view(template_name='contact/logout.html'), name='Index'),
-    path('register1/',  Index.as_view(template_name='contact/register1.html'), name='Index'),
-    path('register2/',  Index.as_view(template_name='contact/register2.html'), name='Index'),
-    path('register3/',  Index.as_view(template_name='contact/register3.html'), name='Index'),
+    path('login/',  Index.as_view(template_name='contact/login.html'), name='login'),
+    path('logout/',  Index.as_view(template_name='contact/logout.html'), name='logout'),
+    path('register1/',  Index.as_view(template_name='contact/register1.html'), name='register1'),
+    path('register2/',  Index.as_view(template_name='contact/register2.html'), name='register2'),
+    path('register3/',  Index.as_view(template_name='contact/register3.html'), name='register3'),
     path('registered_info/',  Index.as_view(template_name='contact/registered_info.html'), name='Index'),
     path('registered_info_update/',  Index.as_view(template_name='contact/registered_info_update.html'), name='Index'),
 
--- contact_manage/static/contact/js/register.js
+++ contact_manage/static/contact/js/register.js
@@ -1,26 +1,9 @@
-$('#next').click(function(e){
-	e.preventDefault()
-	$('body').addClass('step2');
-	navNext();
-});
-
-$('#complete').click(function(e){
-	e.preventDefault()
-	$('body').removeClass('step2').addClass('step3');
-	navNext();
-});
-
-function navNext() {
-   $('nav .active')
-		 .removeClass('active')
-		 .next()
-		 .addClass('active');
-}
-
-$('#name').on('keyup', function(){
-	$("#title span").addClass('active').text($(this).val() + "!");
-
-	if ($('#name').val().trim().length === 0) {
-		$('#title span').text("").removeClass('active');
-	}
+$(document).ready(function(){
+  $(window).scroll(function(){
+    if($(document).scrollTop()>0){
+      $("#header").addClass("scroll");
+    }else{
+      $("#header").removeClass("scroll");
+    }
+  });
 });
